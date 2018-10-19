a3acbe4db8e881a2e406aa5320da135f331b943f
Med <mehdi.gacem@versusmind.eu>
2018-10-19 15:51:23 +0200

    Application module : add cars module + add HttpModule allowing DI of http in car service + add APPCONFIG class as a provider to allow DI in car service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { APPCONFIG } from './app.config';
+import { CarsModule } from './cars/cars.module';
+import { ForbiddenBrandValidatorDirective } from './cars/shared/forbidden-brand-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForbiddenBrandValidatorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [{provide : APPCONFIG, useValue : APPCONFIG.API_URL}],//APPCONFIG
   bootstrap: [AppComponent]
 })
 export class AppModule { }
