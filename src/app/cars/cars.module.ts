f5027fe16e3b8dd81376336ef3feb2d3c034eecc
Med <mehdi.gacem@versusmind.eu>
2018-10-19 16:40:31 +0200

    Import and declare ForbiddenBrandValidatorDirective in the car module to use the custom validator

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CarsComponent } from './cars.component';
 import { CarCreateComponent } from '../car-create/car-create.component';
 import { FormsModule } from '@angular/forms';
-//import { ForbiddenBrandValidatorDirective } from './shared/forbidden-brand-validator.directive';
+import { ForbiddenBrandValidatorDirective } from './shared/forbidden-brand-validator.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [CarsComponent, CarCreateComponent],//ForbiddenBrandValidatorDirective
+  declarations: [CarsComponent, CarCreateComponent,ForbiddenBrandValidatorDirective],//
   exports: [CarsComponent, CarCreateComponent]
 })
 export class CarsModule { }
